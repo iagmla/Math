babf6c1ea934d67279fe6f1e6193118c0703fae6
Karl Zander <iagmla@nifu.local>
2024-04-29 11:03:02 -0700

    init

---
+++ math/triangular.py
@@ -0,0 +1,10 @@
+import math
+
+def get_nth_triangular(n):
+    return (n * (n + 1)) // 2
+
+def is_triangular(n):
+    t = (int(math.sqrt((8 * n) + 1) - 1)) // 2
+    if t % 2 == 0:
+        return True
+    return False
