52109fe9c344d784474658676e7d1cd0c082a5ff
Karl Zander <iagmla@nifu.local>
2024-05-04 07:24:56 -0700

    modular inverse

--- math/euclid.py
+++ math/euclid.py
@@ -2,7 +2,7 @@
 ''' by Karl Zander '''
 
 def gcd(a, b):
-    while a != 0 or b != 0:
+    while  b != 0:
         try:
             a = a % b
         except ZeroDivisionError:
@@ -10,3 +10,18 @@ def gcd(a, b):
         else:
             a, b = b, a
     return a
+
+def inverse(a, n):
+    r0 = n
+    r1 = a
+    t0 = 0
+    t1 = 1
+    while r1 != 0:
+        q = r0 // r1
+        t0, t1 = t1, t0 - q * t1
+        r0, r1 = r1, r0 - q * r1
+    if r0 > 1:
+        return 0
+    if t0 < 0:
+        t0 = t0 + n
+    return t0
