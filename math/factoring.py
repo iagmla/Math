77b1edacb6f22dd397088e12fc77b6a34545b835
Karl Zander <iagmla@nifu.local>
2024-05-16 22:42:32 -0700

    factoring init

---
+++ math/factoring.py
@@ -0,0 +1,14 @@
+''' Factoring Algorithms '''
+''' by Karl Zander '''
+
+def factor(n):
+    d = 2
+    r = 1
+    x = n
+    while r != 0:
+        x = n // d
+        r = n % d
+        if r != 0:
+            d += 1
+    v = n // d
+    return d, v
