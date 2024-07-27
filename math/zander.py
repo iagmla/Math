18d4a83a36c6c06422b4dc90c7b46fce246e2fe0
Karl Zander <karl.c.zander@gmail.com>
2024-07-26 20:06:32 -0700

    Zander Primality Test

--- math/zander.py
+++ math/zander.py
@@ -1,10 +1,19 @@
 ''' Zander Algorithms '''
 ''' by Karl Zander '''
 
+from euclid import inverse
+
+''' Zander Primality Test '''
+
 def is_prime(n):
-    a = n
-    a = (a * a) + 2
-    b = pow(a, n - 1, n)
-    if b == 1:
+    iters = 1000
+    if n < iters:
+        iters = n
+    zcount = 0
+    for i in range(2, iters):
+        v = inverse(i, n)
+        if v == 0:
+            zcount += 1
+    if zcount == 0:
         return True
     return False
