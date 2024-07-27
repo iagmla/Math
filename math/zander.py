c3377ee38f50ddb6ef95c1705587ddc1459389b0
Karl Zander <karl.c.zander@gmail.com>
2024-07-27 12:24:08 -0700

    random nbit ints and primes

--- math/zander.py
+++ math/zander.py
@@ -17,3 +17,31 @@ def is_prime(n):
     if zcount == 0:
         return True
     return False
+
+''' Generate N bit Integer '''
+''' Bit Length must be divisible by 8 '''
+''' Cannot be greater than Python default maximum of 4300 bits '''
+
+def nbit_integer(n):
+    num_bytes = n // 8
+    f = open("/dev/urandom", "rb")
+    b = f.read(num_bytes)
+    f.close()
+
+    x = 0
+    p = (len(b) * 8) - 8
+    for i in range(len(b)):
+        x += b[i] << p
+        p -= 8
+    return x
+
+''' Generate N bit Prime '''
+''' Bit Length must be divisible by 8 '''
+''' Cannot be greater than Python default maximum of 4300 bits '''
+
+def nbit_prime(n):
+    x = nbit_integer(n)
+
+    while not is_prime(x):
+        x += 1
+    return x
