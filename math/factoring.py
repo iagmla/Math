f00b14e959b22bf4bf95642936e3733c0c8e22eb
Karl Zander <karl.c.zander@gmail.com>
2024-08-17 14:10:08 -0700

    smallest prime factors

--- math/factoring.py
+++ math/factoring.py
@@ -10,3 +10,30 @@ def factor(n):
             d += 1
     v = n // d
     return d, v
+
+''' Factors to the smallest prime factors '''
+''' Produces a list of tuples '''
+
+def zfactor(n):
+    d = 2
+    r = 1
+    f = []
+    while r != 0:
+        r = n % d
+        if r != 0:
+            d += 1
+    v = n // d
+    f.append((d, v))
+    while d == 2:
+        d, v = factor(v)
+        f.append((d, v))
+    t = 0
+    for x in range(0, len(f) - 1):
+        t += f[x][1]
+    a = f[len(f) - 1][0]
+    b = f[len(f) - 1][1]
+    t = (t + (a * b))
+    verified = False
+    if t == n:
+        verified = True
+    return f, verified
